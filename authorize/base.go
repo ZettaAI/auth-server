4fcd7d14b2dda72105ed48a2c47d040fb7e68640
Akhilesh Halageri <akhileshhalageri@gmail.com>
2021-03-19 16:05:49 +0000

    fix: model and policy issues, update casbin version

--- authorize/base.go
+++ authorize/base.go
@@ -1,8 +1,10 @@
 package authorize
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/casbin/casbin/v2"
 	defaultrolemanager "github.com/casbin/casbin/v2/rbac/default-role-manager"
@@ -22,38 +24,36 @@ func Authorize(c echo.Context, email string) error {
 	c.Response().Header().Set("X-Forwarded-User", email)
 	c.Response().Header().Set("X-Forwarded-Domain", domain)
 
-	// authorization model needs more work
-	// disabled for now
-	return c.String(http.StatusOK, "success")
+	if strings.Contains(uri, "logout") {
+		return c.String(http.StatusOK, "success")
+	}
 
-	// if strings.Contains(uri, "logout") {
-	// 	return c.String(http.StatusOK, "success")
-	// }
-
-	// authorized := enforce(email, domain, method, uri)
-	// if !authorized {
-	// 	// not enough permissions 403
-	// 	return c.String(
-	// 		http.StatusForbidden,
-	// 		fmt.Sprintf(
-	// 			"User %v does not have the permission to %v %v", email, method, uri),
-	// 	)
-	// }
-	// return c.String(
-	// 	http.StatusOK,
-	// 	fmt.Sprintf("User %v authorized to %v %v", email, method, uri),
-	// )
+	authorized := enforce(email, domain, method, uri)
+	if !authorized {
+		// not enough permissions 403
+		return c.String(
+			http.StatusForbidden,
+			fmt.Sprintf(
+				"User %s not authorized to %s %s in domain %s", email, method, uri, domain),
+		)
+	}
+	return c.String(
+		http.StatusOK,
+		fmt.Sprintf("User %s authorized to %s %s in domain %s", email, method, uri, domain),
+	)
 }
 
 func enforce(email string, domain string, method string, uri string) bool {
 	e, err := casbin.NewEnforcer("casbin/model.conf", "casbin/policy.csv")
 	if err != nil {
-		log.Fatalf("error: adapter: %s", err)
+		// if authorization model is missing assume every request is authorized
+		log.Println("No policy, allow by default.")
+		return true
 	}
 
 	rm := e.GetRoleManager()
-	rm.(*defaultrolemanager.RoleManager).AddMatchingFunc("regexMatch", util.RegexMatch)
-	// rm.PrintRoles()
+	rm.(*defaultrolemanager.RoleManager).AddMatchingFunc("KeyMatch2", util.KeyMatch2)
+	rm.(*defaultrolemanager.RoleManager).AddDomainMatchingFunc("KeyMatch2", util.KeyMatch2)
 
 	authorized, err := e.Enforce(email, domain, uri, method)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,25 +3,31 @@ module github.com/ZettaAI/auth-server
 go 1.15
 
 require (
-	cloud.google.com/go v0.74.0 // indirect
-	github.com/casbin/casbin/v2 v2.19.5
-	github.com/casbin/xorm-adapter/v2 v2.1.0
+	cloud.google.com/go v0.79.0 // indirect
+	github.com/casbin/casbin/v2 v2.25.5
+	github.com/casbin/xorm-adapter/v2 v2.2.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/go-redis/redis/v8 v8.4.4
+	github.com/go-redis/redis/v8 v8.7.1
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.10.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/nxadm/tail v1.4.5 // indirect
-	github.com/stretchr/testify v1.6.1
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
+	github.com/onsi/ginkgo v1.15.2 // indirect
+	github.com/onsi/gomega v1.11.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/net v0.0.0-20201216054612-986b41b23924 // indirect
-	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
-	golang.org/x/sys v0.0.0-20201221093633-bc327ba9c2f0 // indirect
+	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/oauth2 v0.0.0-20210313182246-cd4f82c27b84
+	golang.org/x/sys v0.0.0-20210319071255-635bc2c9138d // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
-	xorm.io/xorm v1.0.5
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	xorm.io/builder v0.3.9 // indirect
+	xorm.io/xorm v1.0.7
 )
